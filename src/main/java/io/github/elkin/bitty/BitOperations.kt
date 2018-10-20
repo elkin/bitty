68f970759e54f441d6c493310de2e2bbba538f27
Vladimir Elkin <elkin.v.n@gmail.com>
2018-10-20 16:47:18 +0300

    Rename slice methods

--- src/main/java/io/github/elkin/bitty/BitOperations.kt
+++ src/main/java/io/github/elkin/bitty/BitOperations.kt
@@ -2,15 +2,17 @@ package io.github.elkin.bitty
 
 fun Int.getBitsSlice(startIndex: Int, stopIndex: Int) = IntegerUtil.getBitsSlice(this, startIndex, stopIndex)
 
-fun Int.setBitsSLice(startIndex: Int, stopIndex: Int) = IntegerUtil.setBitsSlice(this, startIndex, stopIndex)
+fun Int.setBitsSlice(startIndex: Int, stopIndex: Int) = IntegerUtil.setBitsSlice(this, startIndex, stopIndex)
 
-fun Int.clearBitsSLice(startIndex: Int, stopIndex: Int) = IntegerUtil.clearBitsSlice(this, startIndex, stopIndex)
+fun Int.clearBitsSlice(startIndex: Int, stopIndex: Int) = IntegerUtil.clearBitsSlice(this, startIndex, stopIndex)
 
 fun Int.getBit(index: Int) = IntegerUtil.getBit(this, index)
 
 fun Int.setBit(index: Int) = IntegerUtil.setBit(this, index)
 
 fun Int.clearBit(index: Int) = IntegerUtil.clearBit(this, index)
+
+fun Int.isBitSet(index: Int) = IntegerUtil.isBitSet(this, index)
 
 fun Int.numberOfBytes() = IntegerUtil.numberOfBytes(this)
 
@@ -20,15 +22,17 @@ fun Int.getByte(index: Int) = IntegerUtil.getByte(this, index)
 
 fun Long.getBitsSlice(startIndex: Int, stopIndex: Int) = LongUtil.getBitsSlice(this, startIndex, stopIndex)
 
-fun Long.setBitsSLice(startIndex: Int, stopIndex: Int) = LongUtil.setBitsSlice(this, startIndex, stopIndex)
+fun Long.setBitsSlice(startIndex: Int, stopIndex: Int) = LongUtil.setBitsSlice(this, startIndex, stopIndex)
 
-fun Long.clearBitsSLice(startIndex: Int, stopIndex: Int) = LongUtil.clearBitsSlice(this, startIndex, stopIndex)
+fun Long.clearBitsSlice(startIndex: Int, stopIndex: Int) = LongUtil.clearBitsSlice(this, startIndex, stopIndex)
 
 fun Long.getBit(index: Int) = LongUtil.getBit(this, index)
 
 fun Long.setBit(index: Int) = LongUtil.setBit(this, index)
 
 fun Long.clearBit(index: Int) = LongUtil.clearBit(this, index)
+
+fun Long.isBitSet(index: Int) = LongUtil.isBitSet(this, index)
 
 fun Long.numberOfBytes() = LongUtil.numberOfBytes(this)
 
@@ -38,15 +42,17 @@ fun Long.getByte(index: Int) = LongUtil.getByte(this, index)
 
 fun Short.getBitsSlice(startIndex: Int, stopIndex: Int) = ShortUtil.getBitsSlice(this, startIndex, stopIndex)
 
-fun Short.setBitsSLice(startIndex: Int, stopIndex: Int) = ShortUtil.setBitsSlice(this, startIndex, stopIndex)
+fun Short.setBitsSlice(startIndex: Int, stopIndex: Int) = ShortUtil.setBitsSlice(this, startIndex, stopIndex)
 
-fun Short.clearBitsSLice(startIndex: Int, stopIndex: Int) = ShortUtil.clearBitsSlice(this, startIndex, stopIndex)
+fun Short.clearBitsSlice(startIndex: Int, stopIndex: Int) = ShortUtil.clearBitsSlice(this, startIndex, stopIndex)
 
 fun Short.getBit(index: Int) = ShortUtil.getBit(this, index)
 
 fun Short.setBit(index: Int) = ShortUtil.setBit(this, index)
 
 fun Short.clearBit(index: Int) = ShortUtil.clearBit(this, index)
+
+fun Short.isBitSet(index: Int) = ShortUtil.isBitSet(this, index)
 
 fun Short.numberOfBytes() = ShortUtil.numberOfBytes(this)
 

a13a8a2302f8ea0d2d25c8e9932ed2762dab3de1
Vladimir Elkin <elkin.v.n@gmail.com>
2018-10-20 15:16:16 +0300

    Use shorter from Kotlin functions

--- src/main/java/io/github/elkin/bitty/Util.kt
+++ src/main/java/io/github/elkin/bitty/Util.kt
@@ -1,134 +1,55 @@
 package io.github.elkin.bitty
 
-fun Int.getBitsSlice(startIndex: Int, stopIndex: Int): Int
-{
-    return IntegerUtil.getBitsSlice(this, startIndex, stopIndex)
-}
+fun Int.getBitsSlice(startIndex: Int, stopIndex: Int) = IntegerUtil.getBitsSlice(this, startIndex, stopIndex)
 
-fun Int.setBitsSLice(startIndex: Int, stopIndex: Int): Int{
-    return IntegerUtil.setBitsSlice(this, startIndex, stopIndex)
-}
+fun Int.setBitsSLice(startIndex: Int, stopIndex: Int) = IntegerUtil.setBitsSlice(this, startIndex, stopIndex)
 
-fun Int.clearBitsSLice(startIndex: Int, stopIndex: Int): Int{
-    return IntegerUtil.clearBitsSlice(this, startIndex, stopIndex)
-}
+fun Int.clearBitsSLice(startIndex: Int, stopIndex: Int) = IntegerUtil.clearBitsSlice(this, startIndex, stopIndex)
 
-fun Int.getBit(index: Int): Int
-{
-    return IntegerUtil.getBit(this, index)
-}
+fun Int.getBit(index: Int) = IntegerUtil.getBit(this, index)
 
-fun Int.setBit(index: Int): Int
-{
-    return IntegerUtil.setBit(this, index)
-}
+fun Int.setBit(index: Int) = IntegerUtil.setBit(this, index)
 
-fun Int.clearBit(index: Int): Int
-{
-    return IntegerUtil.clearBit(this, index)
-}
+fun Int.clearBit(index: Int) = IntegerUtil.clearBit(this, index)
 
-fun Int.numberOfBytes(): Int
-{
-    return IntegerUtil.numberOfBytes(this)
-}
+fun Int.numberOfBytes() = IntegerUtil.numberOfBytes(this)
 
-fun Int.clearHighBytes(numBytes: Int): Int
-{
-    return IntegerUtil.clearHighBytes(this, numBytes)
-}
+fun Int.clearHighBytes(numBytes: Int) = IntegerUtil.clearHighBytes(this, numBytes)
 
-fun Int.getByte(index: Int): Byte
-{
-    return IntegerUtil.getByte(this, index)
-}
+fun Int.getByte(index: Int) = IntegerUtil.getByte(this, index)
 
-fun Long.getBitsSlice(startIndex: Int, stopIndex: Int): Long
-{
-    return LongUtil.getBitsSlice(this, startIndex, stopIndex)
-}
+fun Long.getBitsSlice(startIndex: Int, stopIndex: Int) = LongUtil.getBitsSlice(this, startIndex, stopIndex)
 
-fun Long.setBitsSLice(startIndex: Int, stopIndex: Int): Long
-{
-    return LongUtil.setBitsSlice(this, startIndex, stopIndex)
-}
+fun Long.setBitsSLice(startIndex: Int, stopIndex: Int) = LongUtil.setBitsSlice(this, startIndex, stopIndex)
 
-fun Long.clearBitsSLice(startIndex: Int, stopIndex: Int): Long
-{
-    return LongUtil.clearBitsSlice(this, startIndex, stopIndex)
-}
+fun Long.clearBitsSLice(startIndex: Int, stopIndex: Int) = LongUtil.clearBitsSlice(this, startIndex, stopIndex)
 
-fun Long.getBit(index: Int): Long
-{
-    return LongUtil.getBit(this, index)
-}
+fun Long.getBit(index: Int) = LongUtil.getBit(this, index)
 
-fun Long.setBit(index: Int): Long
-{
-    return LongUtil.setBit(this, index)
-}
+fun Long.setBit(index: Int) = LongUtil.setBit(this, index)
 
-fun Long.clearBit(index: Int): Long
-{
-    return LongUtil.clearBit(this, index)
-}
+fun Long.clearBit(index: Int) = LongUtil.clearBit(this, index)
 
-fun Long.numberOfBytes(): Int
-{
-    return LongUtil.numberOfBytes(this)
-}
+fun Long.numberOfBytes() = LongUtil.numberOfBytes(this)
 
-fun Long.clearHighBytes(numBytes: Int): Long
-{
-    return LongUtil.clearHighBytes(this, numBytes)
-}
+fun Long.clearHighBytes(numBytes: Int) = LongUtil.clearHighBytes(this, numBytes)
 
-fun Long.getByte(index: Int): Byte
-{
-    return LongUtil.getByte(this, index)
-}
+fun Long.getByte(index: Int) = LongUtil.getByte(this, index)
 
-fun Short.getBitsSlice(startIndex: Int, stopIndex: Int): Short
-{
-    return ShortUtil.getBitsSlice(this, startIndex, stopIndex)
-}
+fun Short.getBitsSlice(startIndex: Int, stopIndex: Int) = ShortUtil.getBitsSlice(this, startIndex, stopIndex)
 
-fun Short.setBitsSLice(startIndex: Int, stopIndex: Int): Short
-{
-    return ShortUtil.setBitsSlice(this, startIndex, stopIndex)
-}
+fun Short.setBitsSLice(startIndex: Int, stopIndex: Int) = ShortUtil.setBitsSlice(this, startIndex, stopIndex)
 
-fun Short.clearBitsSLice(startIndex: Int, stopIndex: Int): Short
-{
-    return ShortUtil.clearBitsSlice(this, startIndex, stopIndex)
-}
+fun Short.clearBitsSLice(startIndex: Int, stopIndex: Int) = ShortUtil.clearBitsSlice(this, startIndex, stopIndex)
 
-fun Short.getBit(index: Int): Short
-{
-    return ShortUtil.getBit(this, index)
-}
+fun Short.getBit(index: Int) = ShortUtil.getBit(this, index)
 
-fun Short.setBit(index: Int): Short
-{
-    return ShortUtil.setBit(this, index)
-}
+fun Short.setBit(index: Int) = ShortUtil.setBit(this, index)
 
-fun Short.clearBit(index: Int): Short
-{
-    return ShortUtil.clearBit(this, index)
-}
+fun Short.clearBit(index: Int) = ShortUtil.clearBit(this, index)
 
-fun Short.numberOfBytes(): Int
-{
-    return ShortUtil.numberOfBytes(this)
-}
+fun Short.numberOfBytes() = ShortUtil.numberOfBytes(this)
 
-fun Short.clearHighBytes(numBytes: Int): Short
-{
-    return ShortUtil.clearHighBytes(this, numBytes)
-}
+fun Short.clearHighBytes(numBytes: Int) = ShortUtil.clearHighBytes(this, numBytes)
 
-fun Short.getByte(index: Int): Byte
-{
-    return ShortUtil.getByte(this, index)
-}
+fun Short.getByte(index: Int) = ShortUtil.getByte(this, index)
